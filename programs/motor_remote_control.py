d29c4a1df97b8ef98e66ecaf776d13ed45d7d6a9
Marc <marclucchini@gmail.com>
2019-01-25 17:33:07 +0000

    Support for 4 stepper motors

--- programs/motor_remote_control.py
+++ programs/motor_remote_control.py
@@ -4,6 +4,7 @@ from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_Step
 
 
 mh = Adafruit_MotorHAT()
+mh2 = Adafruit_MotorHAT(addr=0x61)
 
 
 def turn_off_motors():
@@ -11,11 +12,19 @@ def turn_off_motors():
     mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
     mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
     mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+    mh2.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+    mh2.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+    mh2.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+    mh2.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
 
 
 atexit.register(turn_off_motors)
-myStepper1 = mh.getStepper(200, 1)
-myStepper2 = mh.getStepper(200, 2)
+steppers = {
+    1: mh.getStepper(200, 1),
+    2: mh.getStepper(200, 2),
+    3: mh2.getStepper(200, 1),
+    4: mh2.getStepper(200, 2)
+}
 
 
 def move_worker(stepper, direction, steps):
@@ -30,10 +39,10 @@ def move(stepper, direction, steps):
 
 
 def input_move():
-    user_input = input("Choose a motor: (1) or (2) and a number of steps (n)\n")
+    user_input = input("Choose a motor: (1), (2), (3) or (4) and a number of steps (n)\n")
 
     stepper_index, steps = user_input.split(" ")
-    stepper = myStepper1 if stepper_index == "1" else myStepper2
+    stepper = steppers.get(int(stepper_index), lambda: 1)
     direction = Adafruit_MotorHAT.FORWARD if int(steps) > 0 else Adafruit_MotorHAT.BACKWARD
     steps = abs(int(steps))
 
